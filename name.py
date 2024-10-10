237828265585261f90d7d646e9fa549fe9c3581c
unknown <altafali786786feroz@gmai.com>
2024-10-10 16:36:53 +0530

    assesment1

---
+++ name.py
@@ -0,0 +1,15 @@
+firstname='syed'
+lastname='althaf'
+age=21
+ssn=97910
+height=5.5
+weight=50.5
+def name():
+    print("firstname:",firstname)
+    print("lastname:",lastname)
+    print("age:",age)
+    print("ssn:",ssn)
+    print("height:",height)
+    print("weight:",weight)
+name()
+
